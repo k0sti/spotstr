19d5ce31dae6f89b73b8c3b059d8c4367ce82efb
kosti <koshdot@proton.me>
2025-09-21 20:17:27 +0100

    capacitor init

---
+++ android/app/src/main/java/space/nexel/spotstr/MainActivity.java
@@ -0,0 +1,5 @@
+package space.nexel.spotstr;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'space.nexel.spotstr',
+  appName: 'Spotstr',
+  webDir: 'dist'
+};
+
+export default config;
